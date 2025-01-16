f21e04a02b3cde8f1923f0b01ea95f3fd8f9ae1a
HealthyDoggo <98058246+HealthyDoggo@users.noreply.github.com>
2025-01-16 22:52:10 +0000

    added flush=True to all print statements to ensure output.txt is immediately filled

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ with open('output.txt', 'w') as stdout, open('errors.txt', 'w') as stderr:
         pending_responses = list(load_pending_responses())
         linked_accounts = load_linked_accounts()
 
-
         console_queue = asyncio.Queue()
-        print("output testing")
+        print("output testing", flush=True)
+
         async def handle_player_response(message: discord.Message, address):
             if message.content.lower() == 'yes':
                 await message.author.send(f"Thank you for confirming. Your login from IP {address} is authorised.")
                 MCusername = [username for username, discord_id in linked_accounts.items() if discord_id == message.author.id][0]
                 add_ip_address(MCusername, address)
                 await console_queue.put({"action": "command", "text": f"pardon-ip {address}"})
-                print(pending_responses)
+                print(pending_responses, flush=True)
                 pending_responses.remove([message.author.id, address])
             elif message.content.lower() == 'no':
                 await message.author.send(f"Thank you. The login attempt from IP {address} has been blocked and the IP banned.")
@@ -39,6 +39,7 @@ with open('output.txt', 'w') as stdout, open('errors.txt', 'w') as stderr:
             else:
                 await message.author.send("This was an invalid response. Please reply with 'yes' or 'no'.")
             save_pending_responses(pending_responses)
+
         @tree.command(
                 name="start", 
                 description="starts the Aternos server if not already on.",
@@ -53,8 +54,9 @@ with open('output.txt', 'w') as stdout, open('errors.txt', 'w') as stderr:
                 else:
                     await interaction.followup.send('Error: server already running')
             except Exception as e:
-                print(f"Error in start server main function: {e}")
+                print(f"Error in start server main function: {e}", flush=True)
                 await interaction.followup.send("An error occurred. Please try again.\n {e}")
+
         # @tree.command(
         #         name="uptime",
         #         description="note: only works when server started by bot.",
@@ -70,6 +72,7 @@ with open('output.txt', 'w') as stdout, open('errors.txt', 'w') as stderr:
         #             await interaction.followup.send(f'The server has been on for {time.gmtime(timeStarted-time.time())}')
         #     except:
         #         await interaction.followup.send("An error occured. Please try again.")
+
         @tree.command(
                 name="status",
                 description="displays the current status of the aternos server.",
@@ -83,8 +86,9 @@ with open('output.txt', 'w') as stdout, open('errors.txt', 'w') as stderr:
                 else:
                     await interaction.followup.send('The server is currently off', ephemeral=True)
             except Exception as e:
-                print(f"Error in check status main function: {e}")
+                print(f"Error in check status main function: {e}", flush=True)
                 await interaction.followup.send("An error occurred. Please try again.\n {e}")
+
         @tree.command(
                 name="online",
                 description="Displays the players currently online on the server.",
@@ -101,8 +105,9 @@ with open('output.txt', 'w') as stdout, open('errors.txt', 'w') as stderr:
                 else:
                     await interaction.followup.send("No one is currently online as the server is off.")
             except Exception as e:
-                print(f"Error in display online players main function: {e}")
+                print(f"Error in display online players main function: {e}", flush=True)
                 await interaction.followup.send(f"An error occurred. Please try again. \n {e}")
+
         @tree.command(
                 name="link",
                 description="Connects this discord account to your minecraft account.",
@@ -117,6 +122,7 @@ with open('output.txt', 'w') as stdout, open('errors.txt', 'w') as stderr:
                 linked_accounts[username.lower()] = interaction.user.id
                 await interaction.response.send_message(f"Account {username} successfully linked to discord.")
             save_linked_accounts(linked_accounts)
+
         @tree.command(
                 name="unlink",
                 description="Disconnect this discord account from its connected minecraft account.",
@@ -128,21 +134,24 @@ with open('output.txt', 'w') as stdout, open('errors.txt', 'w') as stderr:
                 del linked_accounts[MCusername]
                 save_linked_accounts(linked_accounts)
                 await interaction.response.send_message(f"Account {MCusername} successfully disconnected from discord.")
+
         @client.event
         async def on_ready():
-            print(f'{client.user} has connected to Discord!')
+            print(f'{client.user} has connected to Discord!', flush=True)
             await tree.sync(guild=discord.Object(id=int(SERVER)))
             asyncio.create_task(maintain_server_tab())
             await asyncio.sleep(7)
             asyncio.create_task(maintain_players_tab())
             await asyncio.sleep(7)
             asyncio.create_task(get_console(console_queue, client, pending_responses, linked_accounts))
+
         @client.event
         async def on_message(message: discord.Message):
             if message.guild is None and not message.author.bot:
                 for id, address in pending_responses:
                     if message.author.id == id and address in message.reference.resolved.content:
                         await handle_player_response(message, address)
+
         if __name__ == "__main__":
             try:             
                 client.run(TOKEN)
@@ -151,11 +160,11 @@ with open('output.txt', 'w') as stdout, open('errors.txt', 'w') as stderr:
                 save_pending_responses(pending_responses)
                 save_linked_accounts(linked_accounts)
                 save_correct_ip_addresses(get_correct_ips())
-                print("Bot stopped and data saved.")
+                print("Bot stopped and data saved.", flush=True)
             except Exception as e:
                 # Handle unexpected exceptions (if needed)
-                print(f"An error occurred: {e}")
+                print(f"An error occurred: {e}", flush=True)
                 save_pending_responses(pending_responses)
                 save_linked_accounts(linked_accounts)
                 save_correct_ip_addresses(get_correct_ips())
-                print("Unexpected error occurred, data saved.")
+                print("Unexpected error occurred, data saved.", flush=True)
